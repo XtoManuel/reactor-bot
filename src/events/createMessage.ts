6229d4300fe8c65cfc88ea6a53d2f16e8551742f
XtoMHA96 <xtomanuel.gamer@gmail.com>
2026-08-22 19:56:19 +0100

    update

--- src/events/createMessage.ts
+++ src/events/createMessage.ts
@@ -10,39 +10,35 @@ import type { CustomClient } from "../types/CustomClient.js";
 
 const COMMANDLESS_DISABLE_EMOJIS = ["💬", "🗨️", "<:pensive_speech_balloon:555620427742052365>"];
 
-export const event = {
-    name: "messageCreate",
-
-    async run(message: Message) {
-        // Ignorar mensajes de bots
-        if (message.author.bot) {
-            return;
-        }
-
-        // Solo funcionar dentro de servidores
-        if (!message.guild) {
-            return;
-        }
-
-        // Solo canales de texto
-        if (!message.channel.isTextBased()) {
-            return;
-        }
-
-        const client = message.client as CustomClient;
-
-        // Estos emojis permiten enviar un mensaje normal
-        // dentro de un canal command.
-        if (COMMANDLESS_DISABLE_EMOJIS.some(emoji => message.content.startsWith(emoji))) {
-            return;
-        }
-
-        const commandless = await isCommandlessChannel(client.pool, { channelId: message.channel.id });
-
-        if (!commandless) {
-            return;
-        }
-
-        await createReactionPoll(client, message);
+export default async function createMessage(message: Message) {
+    // Ignorar mensajes de bots
+    if (message.author.bot) {
+        return;
     }
-};
+
+    // Solo funcionar dentro de servidores
+    if (!message.guild) {
+        return;
+    }
+
+    // Solo canales de texto
+    if (!message.channel.isTextBased()) {
+        return;
+    }
+
+    const client = message.client as CustomClient;
+
+    // Estos emojis permiten enviar un mensaje normal
+    // dentro de un canal command.
+    if (COMMANDLESS_DISABLE_EMOJIS.some(emoji => message.content.startsWith(emoji))) {
+        return;
+    }
+
+    const commandless = await isCommandlessChannel(client.pool, { channelId: message.channel.id });
+
+    if (!commandless) {
+        return;
+    }
+
+    await createReactionPoll(client, message);
+}
